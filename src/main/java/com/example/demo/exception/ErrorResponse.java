250fd40f3df495530d3c58d8082749142d2d2ed0
ff1451 <ff1451@gmail.com>
2024-05-31 20:17:49 +0900

    feat : 오류 응답 형식 코드 추가

---
+++ src/main/java/com/example/demo/exception/ErrorResponse.java
@@ -0,0 +1,44 @@
+package com.example.demo.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class ErrorResponse {
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private int status;
+    private String code;
+    private String message;
+    private List<FieldError> errors;
+
+    @Getter
+    public static class FieldError {
+        private String field;
+        private String message;
+
+        @Builder
+        public FieldError(String field, String message) {
+            this.field = field;
+            this.message = message;
+        }
+    }
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    public ErrorResponse(ErrorCode errorCode, List<FieldError> errors) {
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+        this.errors = errors;
+    }
+}
