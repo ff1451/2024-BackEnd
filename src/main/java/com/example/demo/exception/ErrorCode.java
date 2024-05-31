a4a853111b1cb74b61ee2c7fadd0e8f6fdb7c1dd
ff1451 <ff1451@gmail.com>
2024-05-31 12:13:14 +0900

    feat : 오류 처리를 위한 ErrorCode 추가

---
+++ src/main/java/com/example/demo/exception/ErrorCode.java
@@ -0,0 +1,28 @@
+package com.example.demo.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    ARTICLE_NOT_FOUND(404,"NOT FOUND", "해당 게시물을 찾을 수 없습니다."),
+    BOARD_NOT_FOUND(404,"NOT FOUND", "해당 게시판을 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(404, "NOT FOUND", "해당 사용자를 찾을 수 없습니다."),
+
+    MEMBER_EMAIL_EXISTENCE(409,"CONFLICRT", "해당 이메일을 사용한 사용자가 이미 존재합니다."),
+    INVALID_MEMBER_OR_BOARD(400, "BAD REQUEST", "존재하지 않는 사용자, 게시판을 참조하고 있습니다."),
+
+    NULL_VALUE(400,"BAD REQUEST", "요청 중 null 값이 존재합니다."),
+
+    EXIST_ARTICLE(400, "BAD REQUEST", "사용자 혹은 게시판과 연관된 게시물이 존재합니다.");
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
