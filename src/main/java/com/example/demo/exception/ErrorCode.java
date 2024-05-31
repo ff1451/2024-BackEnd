9ae3efd9b842e80d88df493c0cc845066dcbad1c
ff1451 <ff1451@gmail.com>
2024-05-31 20:15:42 +0900

    feat : 오류 처리를 위한 ErrorCode 추가

--- src/main/java/com/example/demo/exception/ErrorCode.java
+++ src/main/java/com/example/demo/exception/ErrorCode.java
@@ -10,11 +10,14 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(404, "NOT FOUND", "해당 사용자를 찾을 수 없습니다."),
 
     MEMBER_EMAIL_EXISTENCE(409,"CONFLICRT", "해당 이메일을 사용한 사용자가 이미 존재합니다."),
-    INVALID_MEMBER_OR_BOARD(400, "BAD REQUEST", "존재하지 않는 사용자, 게시판을 참조하고 있습니다."),
+    INVALID_MEMBER(400, "BAD REQUEST", "존재하지 않는 사용자를 참조하고 있습니다."),
+    INVALID_BOARD(400, "BAD REQUEST", "존재하지 않는 게시판을 참조하고 있습니다."),
 
     NULL_VALUE(400,"BAD REQUEST", "요청 중 null 값이 존재합니다."),
 
-    EXIST_ARTICLE(400, "BAD REQUEST", "사용자 혹은 게시판과 연관된 게시물이 존재합니다.");
+    EXIST_ARTICLE(400, "BAD REQUEST", "사용자 혹은 게시판과 연관된 게시물이 존재합니다."),
+
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR","서버 오류가 발생했습니다.");
 
     private final int status;
     private final String code;
