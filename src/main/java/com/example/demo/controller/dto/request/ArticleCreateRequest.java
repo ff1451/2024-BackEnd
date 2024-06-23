e047da19c39664490a96405ce8970e8494b8e3f2
ff1451 <ff1451@gmail.com>
2024-06-23 17:28:03 +0900

    refactor : Validation 변경

--- src/main/java/com/example/demo/controller/dto/request/ArticleCreateRequest.java
+++ src/main/java/com/example/demo/controller/dto/request/ArticleCreateRequest.java
@@ -1,15 +1,15 @@
 package com.example.demo.controller.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record ArticleCreateRequest(
-        @NotNull(message = "작성자 ID를 입력해주세요")
+        @NotBlank(message = "작성자 ID를 입력해주세요")
         Long authorId,
-        @NotNull(message = "게시판 ID를 입력해주세요")
+        @NotBlank(message = "게시판 ID를 입력해주세요")
         Long boardId,
-        @NotNull(message = "제목을 입력해주세요")
+        @NotBlank(message = "제목을 입력해주세요")
         String title,
-        @NotNull(message = "내용을 입력해주세요")
+        @NotBlank(message = "내용을 입력해주세요")
         String description
 ) {
 
--- src/main/java/com/example/demo/controller/dto/request/BoardCreateRequest.java
+++ src/main/java/com/example/demo/controller/dto/request/BoardCreateRequest.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record BoardCreateRequest(
-    @NotNull(message = "게시판 명을 입력해주세요")
+    @NotBlank(message = "게시판 명을 입력해주세요")
     String name
 
 ) {
